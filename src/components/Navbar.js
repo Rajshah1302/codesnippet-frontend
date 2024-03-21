9ef55ac9f8903b9779e490c4b6becb928820d2db
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2024-03-21 13:53:14 +0530

    minor update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div className="container-fluid d-flex justify-content-center">
-        <a href="/" className="btn btn-outline-dark mx-2">Upload</a>
-        <a href="/display" className="btn btn-outline-dark mx-2">Display</a>
+        <a href="/" className="btn  mx-2">Upload</a>
+        <a href="/display" className="btn  mx-2">Display</a>
       </div>
     </nav>
   );
